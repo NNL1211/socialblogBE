b55d0a1f40db8b56d5901b2ca353a716c707890e
NNL1211 <nnhatlam1211@Nhats-MacBook-Pro.local>
2021-05-10 17:41:37 +0700

    Added final

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require("mongoose")
 const indexRouter = require('./routes/index');
+const cors = require("cors")
 require("dotenv").config();
 
 const app = express();
@@ -26,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/api', indexRouter);
 
 
